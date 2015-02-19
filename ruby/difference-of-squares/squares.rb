33e381d03b07a3006b11ad954c0dab0f4dc3a006
David Wilkinson <david.wilkinson1984@gmail.com>
2015-02-19 12:31:05 +0000

    tidied it up to put the calculations in methods outside of the initialize method

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -2,20 +2,20 @@ class Squares
   attr_accessor :num, :square_of_sums, :sum_of_squares, :difference
   def initialize(num)
     @num = num
-    @square_of_sums = (1..@num).reduce(:+) ** 2
-    @sum_of_squares = (1..@num).inject { |sum, n| sum + (n ** 2) }
-    @difference = @square_of_sums - @sum_of_squares
+    @square_of_sums = square_sums
+    @sum_of_squares = sum_squares
+    @difference = diff
   end
 
-  # def square_of_sums
-  #   (1..@num).reduce(:+) ** 2
-  # end
+  def square_sums
+    (1..num).reduce(:+) ** 2
+  end
 
-  # def sum_of_squares
-  #   (1..@num).inject { |sum, n| sum + (n ** 2) }
-  # end
+  def sum_squares
+    (1..num).inject { |sum, n| sum + (n ** 2) }
+  end
 
-  # def difference
-  #   square_of_sums - sum_of_squares
-  # end
+  def diff
+    (square_of_sums - sum_of_squares).abs
+  end
 end
