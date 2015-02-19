2c77e8f977ed3737e026c414dd0cff0a451d4a89
David Wilkinson <david.wilkinson1984@gmail.com>
2015-02-19 23:54:32 +0000

    refactored set_name method to remove loops

--- ruby/robot-name/robot.rb
+++ ruby/robot-name/robot.rb
@@ -5,9 +5,7 @@ class Robot
   end
 
   def set_name
-    alpha = 2.times.map { ('A'..'Z').to_a.sample }.join
-    numeric = 3.times.map { ('0'..'9').to_a.sample }.join
-    alpha + numeric
+    ('AA'..'ZZ').to_a.sample + ('000'..'999').to_a.sample
   end
 
   def reset
