c1db99371ab6182bd27acae116fe3c521b708c05
David Wilkinson <david.wilkinson1984@gmail.com>
2015-02-19 12:16:35 +0000

    refactored to set the attributes on initialise, rather than recalculate each time the method is called

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -1,18 +1,21 @@
 class Squares
-  attr_accessor :num
+  attr_accessor :num, :square_of_sums, :sum_of_squares, :difference
   def initialize(num)
     @num = num
+    @square_of_sums = (1..@num).reduce(:+) ** 2
+    @sum_of_squares = (1..@num).inject { |sum, n| sum + (n ** 2) }
+    @difference = @square_of_sums - @sum_of_squares
   end
 
-  def square_of_sums
-    (1..@num).inject { |sum, n| sum + n } ** 2
-  end
+  # def square_of_sums
+  #   (1..@num).reduce(:+) ** 2
+  # end
 
-  def sum_of_squares
-    (1..@num).inject { |sum, n| sum + (n ** 2) }
-  end
+  # def sum_of_squares
+  #   (1..@num).inject { |sum, n| sum + (n ** 2) }
+  # end
 
-  def difference
-    square_of_sums - sum_of_squares
-  end
+  # def difference
+  #   square_of_sums - sum_of_squares
+  # end
 end
