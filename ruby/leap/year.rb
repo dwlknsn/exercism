d68a2c8f868922f27a37fc26ff41f824f5d5ab17
David Wilkinson <david.wilkinson1984@gmail.com>
2015-02-22 22:25:14 +0000

    all tests pass v2

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,8 +1,8 @@
 class Year
   def self.leap?(year)
-    if year % 4 == 0 && year % 400 == 0
+    if year % 400 == 0
       true
-    elsif year % 4 == 0 and year % 100 == 0
+    elsif year % 100 == 0
       false
     elsif year % 4 == 0
       true
